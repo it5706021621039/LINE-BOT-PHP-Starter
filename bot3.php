79ce9cf9af0fd556e2d3ea075ee316c2e1d6362c
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-15 15:36:24 +0700

    a

---
+++ bot3.php
@@ -0,0 +1,16 @@
+<?php
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+{
+    "type": "text",
+    "text": "Hello, world 􀂲"
+}
+
+echo "eiei";
