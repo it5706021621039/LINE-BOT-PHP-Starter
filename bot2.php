805a7fbb8d864ce0f871e5e2bc178b4153d08b07
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-15 14:19:15 +0700

    btn7

--- bot2.php
+++ bot2.php
@@ -48,31 +48,13 @@ if (!is_null($events['events'])) {
 
 			elseif ($text == 'button') {
 				$message=[
-					"type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "http://www.van-huynh.com/img/about/1.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://google.com"
-          }
-      ]
-  }
+					'type'=> 'template',
+		  			'altText'=> 'this is a buttons template',
+		  			'template'=> {
+		      	'type'=> 'buttons',
+		      	'thumbnailImageUrl'=> 'http://www.van-huynh.com/img/about/1.jpg',
+		      	'title'=> "Menu",
+		      	'text'=> 'Please select'
 				];
 			}
 
