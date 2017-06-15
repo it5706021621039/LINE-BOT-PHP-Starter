cafdc91022fa838c8561d0edf7427a1f87738946
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-15 13:16:56 +0700

    echo

--- bot2.php
+++ bot2.php
@@ -33,10 +33,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			echo $text;
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
+
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
