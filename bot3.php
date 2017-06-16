36777f10e1d52a19e28a76547496d1982c6e9fe4
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-16 10:15:12 +0700

    8

--- bot3.php
+++ bot3.php
@@ -1,153 +1,119 @@
 <?php
-$access_token = 'kCz7tuLk8Ox8ptZvYvLn0tB5dIZKkfQwFceq5SVNVlst6gkBC5R6N8pnxEcyp4oZASv1VeAMXO9d7zgx7FHV53qViU7G/4V1lRdTbFvg7aDxHNTFLDZYKBXO2STB6FhrYU07LyObdn3rQ14qbxe1kAdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-			$replyToken = $event['replyToken'];
-			$messages = [
-				'type' => 'sticker',
-				'packageId'=> '1',
-  			'stickerId'=> '1'
-			];
-		}
-		if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-			$replyToken = $event['replyToken'];
-			$messages = [
-				'type'=> 'image',
-  			'originalContentUrl'=> 'https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg',
-    		'previewImageUrl'=> 'https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg'
-			];
-		}
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-      switch ($text) {
-            case 'profile':
-                $userId = $this->textMessage->getUserId();
-                $this->sendProfile($replyToken, $userId);
-                break;
-            case 'bye':
-                if ($this->textMessage->isRoomEvent()) {
-                    $this->bot->replyText($replyToken, 'Leaving room');
-                    $this->bot->leaveRoom($this->textMessage->getRoomId());
-                    break;
-                }
-                if ($this->textMessage->isGroupEvent()) {
-                    $this->bot->replyText($replyToken, 'Leaving group');
-                    $this->bot->leaveGroup($this->textMessage->getGroupId());
-                    break;
-                }
-                $this->bot->replyText($replyToken, 'Bot cannot leave from 1:1 chat');
-                break;
-            case 'confirm':
-                $this->bot->replyMessage(
-                    $replyToken,
-                    new TemplateMessageBuilder(
-                        'Confirm alt text',
-                        new ConfirmTemplateBuilder('Do it?', [
-                            new MessageTemplateActionBuilder('Yes', 'Yes!'),
-                            new MessageTemplateActionBuilder('No', 'No!'),
-                        ])
-                    )
-                );
-                break;
-            case 'buttons':
-                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
-                $buttonTemplateBuilder = new ButtonTemplateBuilder(
-                    'My button sample',
-                    'Hello my button',
-                    $imageUrl,
+namespace LINE\Tests\LINEBot;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\ActionType;
+use LINE\LINEBot\Constant\MessageType;
+use LINE\LINEBot\Constant\TemplateType;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\Tests\LINEBot\Util\DummyHttpClient;
+class SendTemplateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReplyTemplate()
+    {
+        $mock = function ($testRunner, $httpMethod, $url, $data) {
+            /** @var \PHPUnit_Framework_TestCase $testRunner */
+            $testRunner->assertEquals('POST', $httpMethod);
+            $testRunner->assertEquals('https://api.line.me/v2/bot/message/reply', $url);
+            $testRunner->assertEquals('REPLY-TOKEN', $data['replyToken']);
+            $testRunner->assertEquals(1, count($data['messages']));
+            $message = $data['messages'][0];
+            $testRunner->assertEquals(MessageType::TEMPLATE, $message['type']);
+            $testRunner->assertEquals('alt test', $message['altText']);
+            $template = $message['template'];
+            $testRunner->assertEquals(TemplateType::BUTTONS, $template['type']);
+            $testRunner->assertEquals('button title', $template['title']);
+            $testRunner->assertEquals('button button', $template['text']);
+            $testRunner->assertEquals('https://example.com/thumbnail.jpg', $template['thumbnailImageUrl']);
+            $actions = $template['actions'];
+            $testRunner->assertEquals(3, count($actions));
+            $testRunner->assertEquals(ActionType::POSTBACK, $actions[0]['type']);
+            $testRunner->assertEquals('postback label', $actions[0]['label']);
+            $testRunner->assertEquals('post=back', $actions[0]['data']);
+            $testRunner->assertEquals(ActionType::MESSAGE, $actions[1]['type']);
+            $testRunner->assertEquals('message label', $actions[1]['label']);
+            $testRunner->assertEquals('test message', $actions[1]['text']);
+            $testRunner->assertEquals(ActionType::URI, $actions[2]['type']);
+            $testRunner->assertEquals('uri label', $actions[2]['label']);
+            $testRunner->assertEquals('https://example.com', $actions[2]['uri']);
+            return ['status' => 200];
+        };
+        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => '13f5715bad77b642878873bef93b3247']);
+        $res = $bot->replyMessage(
+            'REPLY-TOKEN',
+            new LINEBot\MessageBuilder\TemplateMessageBuilder(
+                'alt test',
+                new ButtonTemplateBuilder(
+                    'button title',
+                    'button button',
+                    'https://example.com/thumbnail.jpg',
                     [
-                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
-                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
-                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
-                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
+                        new PostbackTemplateActionBuilder('postback label', 'post=back'),
+                        new MessageTemplateActionBuilder('message label', 'test message'),
+                        new UriTemplateActionBuilder('uri label', 'https://example.com'),
                     ]
-                );
-                $templateMessage = new TemplateMessageBuilder('Button alt text', $buttonTemplateBuilder);
-                $this->bot->replyMessage($replyToken, $templateMessage);
-                break;
-            case 'carousel':
-                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
-                $carouselTemplateBuilder = new CarouselTemplateBuilder([
-                    new CarouselColumnTemplateBuilder('foo', 'bar', $imageUrl, [
-                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
-                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
-                    ]),
-                    new CarouselColumnTemplateBuilder('buz', 'qux', $imageUrl, [
-                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
-                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
-                    ]),
-                ]);
-                $templateMessage = new TemplateMessageBuilder('Button alt text', $carouselTemplateBuilder);
-                $this->bot->replyMessage($replyToken, $templateMessage);
-                break;
-            case 'imagemap':
-                $richMessageUrl = UrlBuilder::buildUrl($this->req, ['static', 'rich']);
-                $imagemapMessageBuilder = new ImagemapMessageBuilder(
-                    $richMessageUrl,
-                    'This is alt text',
-                    new BaseSizeBuilder(1040, 1040),
+                )
+            )
+        );
+        $this->assertEquals(200, $res->getHTTPStatus());
+        $this->assertTrue($res->isSucceeded());
+        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
+    }
+    public function testPushTemplate()
+    {
+        $mock = function ($testRunner, $httpMethod, $url, $data) {
+            /** @var \PHPUnit_Framework_TestCase $testRunner */
+            $testRunner->assertEquals('POST', $httpMethod);
+            $testRunner->assertEquals('https://api.line.me/v2/bot/message/push', $url);
+            $testRunner->assertEquals('DESTINATION', $data['to']);
+            $testRunner->assertEquals(1, count($data['messages']));
+            $message = $data['messages'][0];
+            $testRunner->assertEquals(MessageType::TEMPLATE, $message['type']);
+            $testRunner->assertEquals('alt test', $message['altText']);
+            $template = $message['template'];
+            $testRunner->assertEquals(TemplateType::BUTTONS, $template['type']);
+            $testRunner->assertEquals('button title', $template['title']);
+            $testRunner->assertEquals('button button', $template['text']);
+            $testRunner->assertEquals('https://example.com/thumbnail.jpg', $template['thumbnailImageUrl']);
+            $actions = $template['actions'];
+            $testRunner->assertEquals(4, count($actions));
+            $testRunner->assertEquals(ActionType::POSTBACK, $actions[0]['type']);
+            $testRunner->assertEquals('postback label', $actions[0]['label']);
+            $testRunner->assertEquals('post=back', $actions[0]['data']);
+            $testRunner->assertEquals(ActionType::POSTBACK, $actions[1]['type']);
+            $testRunner->assertEquals('postback label2', $actions[1]['label']);
+            $testRunner->assertEquals('post=back2', $actions[1]['data']);
+            $testRunner->assertEquals(ActionType::MESSAGE, $actions[2]['type']);
+            $testRunner->assertEquals('message label', $actions[2]['label']);
+            $testRunner->assertEquals('test message', $actions[2]['text']);
+            $testRunner->assertEquals(ActionType::URI, $actions[3]['type']);
+            $testRunner->assertEquals('uri label', $actions[3]['label']);
+            $testRunner->assertEquals('https://example.com', $actions[3]['uri']);
+            return ['status' => 200];
+        };
+        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => '13f5715bad77b642878873bef93b3247']);
+        $res = $bot->pushMessage(
+            'DESTINATION',
+            new LINEBot\MessageBuilder\TemplateMessageBuilder(
+                'alt test',
+                new ButtonTemplateBuilder(
+                    'button title',
+                    'button button',
+                    'https://example.com/thumbnail.jpg',
                     [
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/manga/en',
-                            new AreaBuilder(0, 0, 520, 520)
-                        ),
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/music/en',
-                            new AreaBuilder(520, 0, 520, 520)
-                        ),
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/play/en',
-                            new AreaBuilder(0, 520, 520, 520)
-                        ),
-                        new ImagemapMessageActionBuilder(
-                            'URANAI!',
-                            new AreaBuilder(520, 520, 520, 520)
-                        )
+                        new PostbackTemplateActionBuilder('postback label', 'post=back'),
+                        new PostbackTemplateActionBuilder('postback label2', 'post=back2', 'extend text'),
+                        new MessageTemplateActionBuilder('message label', 'test message'),
+                        new UriTemplateActionBuilder('uri label', 'https://example.com'),
                     ]
-                );
-                $this->bot->replyMessage($replyToken, $imagemapMessageBuilder);
-                break;
-            default:
-                $this->echoBack($replyToken, $text);
-                break;
-              }
-
-
-			}
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$this],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-
-	}
+                )
+            )
+        );
+        $this->assertEquals(200, $res->getHTTPStatus());
+        $this->assertTrue($res->isSucceeded());
+        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
+    }
 }
-echo "Can";
