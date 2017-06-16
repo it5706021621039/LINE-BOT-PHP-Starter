c1806fb9be13815bfa16d2b4fae359735a7754c1
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-16 10:37:47 +0700

    aaa

--- bot3.php
+++ bot3.php
@@ -2,11 +2,4 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('kCz7tuLk8Ox8ptZvYvLn0tB5dIZKkfQwFceq5SVNVlst6gkBC5R6N8pnxEcyp4oZASv1VeAMXO9d7zgx7FHV53qViU7G/4V1lRdTbFvg7aDxHNTFLDZYKBXO2STB6FhrYU07LyObdn3rQ14qbxe1kAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '13f5715bad77b642878873bef93b3247']);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
-
-// Failed
-echo $response->getHTTPStatus . ' ' . $response->getRawBody();
+$response = $bot->replyText('<reply token>', 'hello!');
