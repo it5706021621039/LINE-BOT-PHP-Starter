d512f83079c515699c372ae196d5ab9596cc07b0
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-16 10:45:18 +0700

    2

--- bot3.php
+++ bot3.php
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-require_once('php://input');
+require_once('./LINEBotTiny.php');
 $channelAccessToken = 'kCz7tuLk8Ox8ptZvYvLn0tB5dIZKkfQwFceq5SVNVlst6gkBC5R6N8pnxEcyp4oZASv1VeAMXO9d7zgx7FHV53qViU7G/4V1lRdTbFvg7aDxHNTFLDZYKBXO2STB6FhrYU07LyObdn3rQ14qbxe1kAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '13f5715bad77b642878873bef93b3247';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
