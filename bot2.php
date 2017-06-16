5450b6a85be1231926cb4714cfdb031d0e16cbde
it5706021621039 <5706021621039@fitm.kmutnb.ac.th>
2017-06-16 09:15:08 +0700

    check

--- bot2.php
+++ bot2.php
@@ -46,12 +46,13 @@ if (!is_null($events['events'])) {
 					'text' => 'กรุณาระบุรหัสสินค้า'
 				];
 			}
-			/*elseif ($text == 'M011-HR2810' || $text == 'M011') {
+			elseif ($text == 'M011-HR2810' || $text == 'M011') {
 				$messages = [
-					'type' => 'text',
-					'text' => 'กรุณาระบุรหัสสินค้า'
+					'type'=> 'image',
+	  			'originalContentUrl'=> 'https://www.ktw.co.th/content/images/thumbs/w_1_0369532_hr-2810-%E0%B8%AA%E0%B9%88%E0%B8%99%E0%B9%82%E0%B8%A3%E0%B8%A3%E0%B8%B5%E0%B9%88.jpeg',
+	    		'previewImageUrl'=> 'https://www.ktw.co.th/content/images/thumbs/w_1_0369532_hr-2810-%E0%B8%AA%E0%B9%88%E0%B8%99%E0%B9%82%E0%B8%A3%E0%B8%A3%E0%B8%B5%E0%B9%88.jpeg'
 				];
-			}*/
+			}
 			else {
 				$messages = [
 					'type' => 'text',
